5c1fa817614818a089c58d1baecf9b9e47389281
primitive6 <tony.staunton@outlook.com>
2012-10-18 10:44:28 +0100

    empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nash::Application.config.secret_token = '22b2ffc8221454db78bdc5038aadbe58ce5d962f8cf13b22795b1203c2020e55b44472df7de2b86ee95483cb2c7557f3f06569fdd256c02f1e1ccb1bb839eaf3'
